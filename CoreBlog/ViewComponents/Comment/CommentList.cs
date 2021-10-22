f0fa31840915f7d0232ea20637d82fe614fbc099
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-23 00:00:58 +0300

    ViewComponents Comment CommentList

---
+++ CoreBlog/ViewComponents/Comment/CommentList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Comment
+{
+    public class CommentList : ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var list = commentManager.List(id);
+            return View(list);
+        }
+    }
+}
