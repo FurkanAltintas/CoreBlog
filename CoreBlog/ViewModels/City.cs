cceaba3ed0c7f438ad16cba786e9a8ebdd65b3e5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-23 20:23:48 +0300

    ViewModels - City

--- CoreBlog/ViewModels/City.cs
+++ CoreBlog/ViewModels/City.cs
@@ -9,32 +9,5 @@ namespace CoreBlog.ViewModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        public List<City> cities = new List<City>
-        {
-            new City
-            {
-                Id = 1,
-                Name = "İstanbul"
-            },
-
-            new City
-            {
-                Id = 2,
-                Name = "İzmir"
-            },
-
-            new City
-            {
-                Id = 3,
-                Name = "Ankara"
-            },
-
-            new City
-            {
-                Id = 4,
-                Name = "Tokat"
-            }
-        };
     }
 }
