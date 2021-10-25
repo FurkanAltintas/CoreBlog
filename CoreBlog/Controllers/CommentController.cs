32640187a965a91ff72f42017049f121765ca4c4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-25 21:41:40 +0300

    Comment Controller

--- CoreBlog/Controllers/CommentController.cs
+++ CoreBlog/Controllers/CommentController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
+using CoreBlog.ViewModels;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,19 +13,23 @@ namespace CoreBlog.Controllers
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentRepository());
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+        CommentKey commentKey = new CommentKey();
         public PartialViewResult List(int id)
         {
             return PartialView(commentManager.List(id));
         }
 
+        [HttpGet]
         public PartialViewResult Add()
         {
             return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Comment p)
+        {
+            commentManager.Add(p);
+            return RedirectToAction("Detail", "Blog", new { id = p.BlogId });
         }
     }
 }
