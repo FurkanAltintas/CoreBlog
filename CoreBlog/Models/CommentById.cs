b5a156dbe85afedb71a94ba5e1989c22237fd737
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-25 21:45:30 +0300

    Models CommentById

---
+++ CoreBlog/Models/CommentById.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Models
+{
+    public class CommentById
+    {
+        public CommentById()
+        {
+            CreateDate = DateTime.Now;
+            IsActive = true;
+        }
+
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
