def77f17ab895cc841a36c0c88d08ac08c4b048c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-25 21:36:14 +0300

    Concrete Comment

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -9,11 +9,17 @@ namespace EntityLayer.Concrete
 {
     public class Comment
     {
+        public Comment()
+        {
+            CreateDate = DateTime.Now;
+            IsActive = false;
+        }
+
         [Key]
         public int CommentId { get; set; }
         public string UserName { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
+        public string Message { get; set; }
         public DateTime CreateDate { get; set; }
         public bool IsActive { get; set; }
 
