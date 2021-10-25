41ec5dc493c9ba07867944fc38c235f4468f3f4c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-25 21:41:17 +0300

    ViewComponents - Blog - BlogListByWriter

---
+++ CoreBlog/ViewComponents/Blog/BlogListByWriter.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Writer
+{
+    public class BlogListByWriter : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            return View(blogManager.FetchWriter(id));
+        }
+    }
+}
