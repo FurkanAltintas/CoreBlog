643aba0b389f25e081f308ce15fe67d24946f745
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-25 21:44:08 +0300

    Migrations

---
+++ DataAccessLayer/Migrations/20211025101605_mig5.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Comments",
+                newName: "Message");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "Comments",
+                newName: "Content");
+        }
+    }
+}
