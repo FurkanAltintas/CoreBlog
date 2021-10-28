02a6878fc3a05d822f6ec0abff73d17981d3ff71
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-28 16:47:59 +0300

    ViewComponents - About- RecentPost

---
+++ CoreBlog/ViewComponents/About/RecentPost.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.About
+{
+    public class RecentPost : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(blogManager.RecentPost(6));
+        }
+    }
+}
