5b25c72c9db61271d8f5867d5cc770f6a9770797
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-28 16:47:27 +0300

    Footer - FooterRecentPost

---
+++ CoreBlog/ViewComponents/Footer/FooterRecentPost.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Footer
+{
+    public class FooterRecentPost : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(blogManager.RecentPost(3));
+        }
+    }
+}
