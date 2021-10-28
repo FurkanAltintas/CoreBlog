cbfe6b55cbb52eaf0247de4dbd3deb6efffd0a0c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-28 14:49:23 +0300

    BusinessLayer

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -13,16 +13,30 @@ namespace BusinessLayer.ValidationRules
         public WriterValidator()
         {
             RuleFor(x => x.FullName).NotEmpty().WithMessage("Yazar adı soyadı boş geçilemez");
+
             RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
+
             RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre boş geçilemez")
                 .MinimumLength(8).WithMessage("Şifreniz en az 8 karakter olmalıdır")
                 .MaximumLength(16).WithMessage("Şifreniz en fazla 16 karakter olmalıdır")
                 .Matches(@"[A-Z]+").WithMessage("Şifrenizde en az 1 büyük harf olmalıdır")
                 .Matches(@"[a-z]+").WithMessage("Şifrenizde en az 1 küçük harf olmalıdır")
                 .Matches(@"[0-9]+").WithMessage("Şifrenizde en az bir rakam olmalıdır");
+
+            RuleFor(x => x.RPassword).NotEmpty().WithMessage("Şifre boş geçilemez")
+                .MinimumLength(8).WithMessage("Şifreniz en az 8 karakter olmalıdır")
+                .MaximumLength(16).WithMessage("Şifreniz en fazla 16 karakter olmalıdır")
+                .Matches(@"[A-Z]+").WithMessage("Şifrenizde en az 1 büyük harf olmalıdır")
+                .Matches(@"[a-z]+").WithMessage("Şifrenizde en az 1 küçük harf olmalıdır")
+                .Matches(@"[0-9]+").WithMessage("Şifrenizde en az bir rakam olmalıdır");
             //1 büyük 1 küçük ve 1 tane sayı olmak zorunda
+
             RuleFor(x => x.FullName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+
             RuleFor(x => x.FullName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik veri girşi yapın");
+
+            RuleFor(x => x.Password).Equal(x => x.RPassword).WithMessage("Parolalar uyuşmuyor");
+            //Kayıt olurken parolanın iki kez girilmesi
         }
     }
 }
