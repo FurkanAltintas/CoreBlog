272ccda0c66401e543554a3acc4b11fbcfdc73b7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-28 16:48:22 +0300

    Contact Controller

---
+++ CoreBlog/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.CreateDate = DateTime.Now;
+            p.IsActive = true;
+            contactManager.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
