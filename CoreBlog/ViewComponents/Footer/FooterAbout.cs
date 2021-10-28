da557d1e8321b84f769bb11b0b11ab3f98a5537f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-28 16:47:45 +0300

    ViewComponents - Footer - FooterAbout

---
+++ CoreBlog/ViewComponents/Footer/FooterAbout.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Footer
+{
+    public class FooterAbout : ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(aboutManager.Bring());
+        }
+    }
+}
