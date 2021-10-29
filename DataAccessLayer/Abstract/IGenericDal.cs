c801c8b8e5c39a71646f1e46960cbaeeb24bd6e6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-29 19:38:41 +0300

    IGenericDal

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -9,21 +9,11 @@ namespace DataAccessLayer.Abstract
 {
     public interface IGenericDal<T> where T : class
     {
-        void Insert(T t); //Ekleme
-        void Delete(T t); //Silme
-        void Update(T t); //Güncelleme
-
-        //List<T> GetListAll();
-
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
         List<T> GetListAll(Expression<Func<T, bool>> filter = null);
-
-        //T GetById(int id);
-
         T GetByBind(Expression<Func<T, bool>> filter = null);
         T GetById(int? id = null);
-
-        //Tek değer getirme ve listeleme filtrelenmiştir.
-        //Insert ve Update tek değer olarak yapılabilir
-        // id > 0 ise Update // id == 0 ise Insert
     }
 }
