a87cf99ab33fe9a04b2e5aac3a4803130558fe7a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-30 22:27:47 +0300

    ViewComponents - Blog - BlogByWriterDashboard

---
+++ CoreBlog/ViewComponents/Blog/BlogByWriterDashboard.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Dashboard
+{
+    public class BlogByWriterDashboard : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            return View(blogManager.FetchCategoryByWriter(1));
+        }
+    }
+}
