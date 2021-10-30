02f372ddb6815da8a97b408e831de6bde68daf33
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-30 22:30:11 +0300

    Statistic Manager

---
+++ BusinessLayer/Concrete/StatisticManager.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class StatisticManager : IStatisticService
+    {
+        IBlogDal _blogDal;
+
+        public StatisticManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public int BlogByWriterCount(int id)
+        {
+            return _blogDal.GetListAll(x => x.WriterId == id).Count();
+        }
+
+        public int BlogCount()
+        {
+            return _blogDal.GetListAll().Count();
+        }
+
+        public int BlogWeekCount()
+        {
+            return _blogDal.GetListAll().Count();
+        }
+    }
+}
