b516a38772f7540fcc25f5306a406cdc229ab55b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-07 15:31:18 +0300

    Category Manager

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -4,6 +4,7 @@ using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,14 +29,23 @@ namespace BusinessLayer.Concrete
             _categoryDal.Delete(t);
         }
 
-        public Category GetById(int id)
+        public Category Get(int id)
         {
-            return _categoryDal.GetById(id);
+            return _categoryDal.Get(id);
         }
 
-        public List<Category> List()
+        public Category Get(Expression<Func<Category, bool>> filter = null)
         {
-            return _categoryDal.GetListAll();
+            return filter == null ?
+                _categoryDal.Get() :
+                _categoryDal.Get(filter);
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter = null)
+        {
+            return filter == null ?
+                _categoryDal.GetListAll() :
+                _categoryDal.GetListAll(filter);
         }
 
         public void Update(Category t)
