59b33c0fba89bdb94fb5ff9c64e33bf81001318a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-07 15:29:33 +0300

    Statistic Service

--- BusinessLayer/Abstract/IStatisticService.cs
+++ BusinessLayer/Abstract/IStatisticService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,8 @@ namespace BusinessLayer.Abstract
     public interface IStatisticService
     {
         int BlogCount();
+        List<Blog> Blog();
         int BlogByWriterCount(int id);
-        int BlogWeekCount();
+        int Category();
     }
 }
