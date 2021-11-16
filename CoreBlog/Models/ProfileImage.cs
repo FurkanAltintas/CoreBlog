e88b89ea60cfd6898f16604d8f88414fcea5bfe7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-16 14:19:39 +0300

    Models - ProfileImage

--- CoreBlog/Models/ProfileImage.cs
+++ CoreBlog/Models/ProfileImage.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,24 @@ namespace CoreBlog.Models
 {
     public class ProfileImage
     {
-        public int WriterId { get; set; }
-        public string FullName { get; set; }
-        public string About { get; set; }
-        public IFormFile Image { get; set; }
-        public string Mail { get; set; }
-        public string Password { get; set; }
-        public string RPassword { get; set; }
-        public DateTime CreateDate { get; set; }
-        public bool IsActive { get; set; }
+        public void Profile(IFormFile formFile, out string fileName)
+        {
+            var extension = Path.GetExtension(formFile.FileName);
+            var imageName = Guid.NewGuid() + extension;
+            var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/image/", imageName);
+            var stream = new FileStream(location, FileMode.Create);
+            formFile.CopyTo(stream);
+            fileName = formFile.FileName;
+        }
+
+        //public int WriterId { get; set; }
+        //public string FullName { get; set; }
+        //public string About { get; set; }
+        //public IFormFile Image { get; set; }
+        //public string Mail { get; set; }
+        //public string Password { get; set; }
+        //public string RPassword { get; set; }
+        //public DateTime CreateDate { get; set; }
+        //public bool IsActive { get; set; }
     }
 }
