032a4c452b8d0427cb98d02a1e6fc0fc4db93893
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-16 14:27:56 +0300

    ViewComponents - Writer - Notification

---
+++ CoreBlog/ViewComponents/Writer/Notification.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.ViewComponents.Writer
+{
+    public class Notification : ViewComponent
+    {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(notificationManager.List(true, 2));
+        }
+    }
+}
