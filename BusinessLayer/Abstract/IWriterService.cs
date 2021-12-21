15418928949261e598b1672e862a7aabf526d032
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:40:15 +0300

    Writer Service

--- BusinessLayer/Abstract/IWriterService.cs
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -9,6 +9,6 @@ namespace BusinessLayer.Abstract
 {
     public interface IWriterService : IGenericService<Writer>
     {
-        bool Login(Writer writer);
+        Writer Login(Writer writer);
     }
 }
