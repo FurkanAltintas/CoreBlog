6c28d8decf04391e60ccfcd962377c3bda031794
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:45:03 +0300

    Category Controller

--- CoreBlog/Controllers/CategoryController.cs
+++ CoreBlog/Controllers/CategoryController.cs
@@ -1,19 +1,20 @@
-﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
+﻿using BusinessLayer.Abstract;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreBlog.Controllers
 {
     public class CategoryController : Controller
     {
-        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
         public IActionResult Index()
         {
-            var list = categoryManager.List();
+            var list = _categoryService.List();
             return View(list);
         }
     }
