1768dc9ebcd6f661e0596a0d69a46d8f410e453e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:45:25 +0300

    Dashboard Controller

--- CoreBlog/Controllers/DashboardController.cs
+++ CoreBlog/Controllers/DashboardController.cs
@@ -1,5 +1,5 @@
 ﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Concrete.EntityFramework.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
