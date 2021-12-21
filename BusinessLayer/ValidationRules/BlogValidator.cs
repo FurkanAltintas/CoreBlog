25b6f6abcd7e4b6d92f800563febc4f8ce910054
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:43:34 +0300

    Blog Validator

--- BusinessLayer/ValidationRules/BlogValidator.cs
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -1,25 +1,25 @@
-﻿using EntityLayer.Concrete;
+﻿using static BusinessLayer.Utilities.ValidatorMessages;
+using BusinessLayer.Utilities;
+using EntityLayer.Concrete;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
 {
     public class BlogValidator : AbstractValidator<Blog>
     {
+        ValidatorMessages _validatorMessages = new ValidatorMessages();
+        Matches matches = new Matches();
         public BlogValidator()
         {
-            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog başlığını boş geçemezsiniz")
-                                 .Length(4,150).WithMessage("Blog başlığı 4 ile 150 karakter arasında olmalıdır");
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog Başlığı" + _validatorMessages.NotEmpty)
+                               .MaximumLength(50).WithMessage("Blog Başlığı {MaxLenght}" + _validatorMessages.MaximumLenght)
+                               .MinimumLength(2).WithMessage("Blog Başlığı {MinLenght}" + _validatorMessages.MinimumLenght);
 
-            RuleFor(x => x.Description).NotEmpty().WithMessage("Blog içeriğini boş geçemezsiniz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Blog İçeriği " + _validatorMessages.NotEmpty);
 
-            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Kategori Adını boş geçemezsiniz");
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Kategori Adı " + _validatorMessages.NotEmpty);
 
-            RuleFor(x => x.Image).NotEmpty().WithMessage("Blog görselini boş geçemezsiniz");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Blog görseli " + _validatorMessages.NotEmpty);
 
         }
     }
