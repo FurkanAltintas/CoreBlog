90f63afc0b4d309c67981e35b1be4a771209b2d8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:45:40 +0300

    Message Controller

---
+++ CoreBlog/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public IActionResult Inbox()
+        {
+            return View(_messageService.GetInboxListByWriter(2));
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_messageService.Get(x => x.MessageId == id));
+        }
+    }
+}
