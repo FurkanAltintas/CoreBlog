cedc4df11bb79185e5f5a97ada87ff900455631c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:48:06 +0300

    EntityFramework Repository

---
+++ DataAccessLayer/Concrete/EntityFramework/Repositories/EfBlogRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete.EntityFramework.Contexts;
+using DataAccessLayer.Abstract.Repositories;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework.Repositories
+{
+    public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
+    {
+        public List<Blog> GetListWithCategory()
+        {
+            using var c = new Context();
+            return c.Blogs.Include(x=>x.Category).ToList();
+        }
+
+        public List<Blog> GetListWithCategoryByWriter(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Blogs.Include(x => x.Category).Where(x => x.WriterId == id).ToList();
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/EntityFramework/Repositories/EfMessageRepository.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Abstract.Repositories;
+using DataAccessLayer.Concrete.EntityFramework.Contexts;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework.Repositories
+{
+    public class EfMessageRepository : GenericRepository<Message>, IMessageDal
+    {
+        public List<Message> GetListWithMessageByWriter(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Messages.Include(x => x.SenderUser).Where(x => x.ReceiverId == id).ToList();
+            }
+        }
+    }
+}
