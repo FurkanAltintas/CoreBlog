1e9f5c5473a539427e675d2a8cb84a2ecbddf0d4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:45:57 +0300

    Notification Controller

--- CoreBlog/Controllers/NotificationController.cs
+++ CoreBlog/Controllers/NotificationController.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
 {
+    [AllowAnonymous]
     public class NotificationController : Controller
     {
+        INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_notificationService.Ordered());
         }
     }
 }
