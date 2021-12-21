b70c5a9eb400d9c8e9f6fb70a2671a1e92297445
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:48:23 +0300

    IUnitOfWork

---
+++ DataAccessLayer/Abstract/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Abstract.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract.UnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAboutDal About { get; }
+        IBlogDal Blog { get; }
+        ICategoryDal Category { get; }
+        ICommentDal Comment { get; }
+        IContactDal Contact { get; }
+        IMessageDal Message { get; }
+        INewsletterDal Newsletter { get; }
+        INotificationDal Notification { get; }
+        IWriterDal Writer { get; }
+        int Save();
+    }
+}
