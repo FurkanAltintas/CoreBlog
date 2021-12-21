94c93add27e69ab22a15e1125b1a13d791c7761e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:48:16 +0300

    Context

--- DataAccessLayer/Concrete/EntityFramework/Contexts/Context.cs
+++ DataAccessLayer/Concrete/EntityFramework/Contexts/Context.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using DataAccessLayer.Mapping;
+using EntityLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,13 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Concrete
+namespace DataAccessLayer.Concrete.EntityFramework.Contexts
 {
     public class Context : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-STRIXV5; database=CoreBlog; integrated security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MessageMap());
+
+            //modelBuilder.Entity<Message>()
+            //       .HasOne(x => x.SenderUser)
+            //       .WithMany(x => x.SenderMessage)
+            //       .HasForeignKey(x => x.SenderId)
+            //       .OnDelete(DeleteBehavior.ClientSetNull);
+
+            //modelBuilder.Entity<Message>()
+            //       .HasOne(x => x.ReceiverUser)
+            //       .WithMany(x => x.ReceiverMessage)
+            //       .HasForeignKey(x => x.ReceiverId)
+            //       .OnDelete(DeleteBehavior.ClientSetNull);
         }
 
         public DbSet<About> Abouts { get; set; }
