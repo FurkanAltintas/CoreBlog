e6fc6a17d9d41b2b1e6b516f4c9900c8e98a69e6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:47:53 +0300

    Message Map

---
+++ DataAccessLayer/Concrete/Mapping/MessageMap.cs
@@ -0,0 +1,32 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mapping
+{
+    public class MessageMap : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(x => x.MessageId);
+            builder.Property(x => x.MessageId).UseIdentityColumn();
+            builder.Property(x => x.MessageId).ValueGeneratedOnAdd();
+
+            builder.HasOne(x => x.SenderUser)
+                   .WithMany(x => x.SenderMessage)
+                   .HasForeignKey(x => x.SenderId)
+                   .OnDelete(DeleteBehavior.ClientSetNull);
+
+
+            builder.HasOne(x => x.ReceiverUser)
+                   .WithMany(x => x.ReceiverMessage)
+                   .HasForeignKey(x => x.ReceiverId)
+                   .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+}
