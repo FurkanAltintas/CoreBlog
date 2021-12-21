dffccb1cc3b084abc39ca515e22c628ef8e1309a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-12-22 00:43:57 +0300

    Base Controller

---
+++ CoreBlog/Base/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Base
+{
+    public class BaseController : Controller
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Mail = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.Email).Value;
+            Id = Convert.ToInt32(((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.Name).Value);
+        }
+    }
+}
